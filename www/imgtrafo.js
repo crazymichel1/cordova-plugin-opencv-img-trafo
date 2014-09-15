3953f5da6550816b5d8843a8e319c62a88ffe5a4
crazymichel1 <2187--4a8>
2014-09-15 17:46:01 +0200

    plugin with native calendar part example

--- www/imgtrafo.js
+++ www/imgtrafo.js
@@ -1,7 +1,33 @@
 var imgtrafo = {
 	test: function() {
-            alert('test');
+		// test values
+		var startDate = new Date("September 24, 2013 8:00:00");
+        var endDate = new Date("September 24, 2013 18:00:00");
+        var title = "PhoneGap Day";
+        var location = "Amsterdam";
+        var notes = "Arrive on time, don't want to miss out!";
+        var success = function() { alert("Success"); };
+        var error = function(message) { alert("Oopsie! " + message); };
+		
+		cordova.exec(
+			successCallback, // success callback function
+            errorCallback, // error callback function
+            'Calendar', // mapped to our native Java class
+            'addCalendarEntry', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "title": title,
+                "description": notes,
+                "eventLocation": location,
+                "startTimeMillis": startDate.getTime(),
+                "endTimeMillis": endDate.getTime()
+            }]
+		);
 	}
+	
+	test2: function() {
+    	alert('test');
+	}
+	
 };
 
-module.exports = imgtrafo;
+module.exports = imgtrafo; // export is important!
