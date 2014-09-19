75334ce802cb6b72b2afc73b2a0814a2115fd163
crazymichel1 <2187--4a8>
2014-09-19 13:59:26 +0200

    bug fix

--- src/android/ImgTrafo.java
+++ src/android/ImgTrafo.java
@@ -22,7 +22,8 @@ import android.util.Log;
 import android.view.SurfaceView;
 
 import android.app.Activity;
-import de.michaelskoehler.imgtrafo.R;
+import android.R;
+//import de.michaelskoehler.imgtrafo.R;
 
 
 public class ImgTrafo extends CordovaPlugin implements CvCameraViewListener2 {
@@ -53,7 +54,7 @@ public class ImgTrafo extends CordovaPlugin implements CvCameraViewListener2 {
     };
     
     
-    public void onCamerahStarted(int width, int height) {
+    public void onCameraViewStarted(int width, int height) {
     	
     }
     
