f3a92cf3b6b65553f163652c8823645da9cb7a42
crazymichel1 <2187--4a8>
2014-09-25 01:46:59 +0200

    opencv commented

--- src/android/OpenCVActivity.java
+++ src/android/OpenCVActivity.java
@@ -75,6 +75,7 @@ public class OpenCVActivity extends Activity {
     		switch (status) {
 	    		case LoaderCallbackInterface.SUCCESS:
 	    		{
+	    			/*
 	    			// OPENCV ACTIONS here:
 	    			// setting image resource from drawable via bitmap
 	    			Bitmap b_input = BitmapFactory.decodeResource(resources, R_drawable_left07);
@@ -89,7 +90,7 @@ public class OpenCVActivity extends Activity {
 	    	 		
 	    	        ImageView imageView = (ImageView) findViewById(R_id_imageView1);
 	    	 		imageView.setImageBitmap(b_output);
-	    	 		
+	    	 		*/
 	    		} break;
 	    		default:
 	    		{
